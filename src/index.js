40ea86a33b7c2ea3288abf97102f8450cabd7001
Kamiouji1 <edmogc@gmail.com>
2024-11-16 17:28:52 -0300

    finalizado a aula 10 - View Engines

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const rotaUsuario = require('./rotas/usuario.rot')
 const rotaPost = require ('./rotas/posts.rota')
+var expressLayouts = require('express-ejs-layouts')
 
 const app = express()
+
 app.use(express.json())
+app.set('view engine', 'ejs')
+
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
 
 app.use('/static', express.static('public'))
 
@@ -14,6 +20,30 @@ app.use('/posts', rotaPost)
 
 app.get('/', (req, res) =>{
     res.json({msg:'Hello from Express!'})
+})
+
+app.get('/home', (req, res) => { // quando o usuario acessar /home
+    const number = Math.random()
+    res.render('pages/index', {variavel: number})    // renderiza a pagina index que ta dentro de pages
+})
+
+app.get('/cursos', (req, res) => { // quando o usuario acessar /home
+    const cursos = [
+        {nome: "Programação Frontend", ch: 280},
+        {nome: "Programação backend", ch: 300},
+        {nome: "Programação SQL", ch: 189}
+    ]
+    res.render('pages/cursos/index', {cursos: cursos})    // renderiza a pagina index que ta dentro de pages
+})
+
+app.get('/alunos', (req, res) => { 
+    const alunos = [
+        {nome: "João"},
+        {nome: "Maria"},
+        {nome: "Pedro"},
+        
+    ]
+    res.render('pages/alunos/index',{alunos: alunos})
 })
 
 
